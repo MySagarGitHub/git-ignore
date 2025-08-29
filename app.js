0a50aae891bdcca723eaadf52b49ccfbc56ea346
MySagarGithub <Pandaysagar2004@gmail.com>
2025-08-29 20:03:29 +0545

    little changes

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express API!');
+});
+
+// Another route
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Here is your data!', data: [1, 2, 3, 4, 5] });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port:3000}`);
+});
+
