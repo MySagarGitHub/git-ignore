9d244f8bfb35b9815ce97d30fdcbb1b636a552fe
MySagarGithub <Pandaysagar2004@gmail.com>
2025-09-09 20:25:15 +0545

    daily contribution

--- page.jsx
+++ page.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function page({children}) {
   return (
-    <div className='bg-gray-100 min-h-screen'>
+    <div className='bg-gray-100 min-h-screen '>
       {children}
     </div>
   )
 }
 export const metadata = {
   title: 'My Page Title',
-  description: 'This is a description of my page',
+  description: 'This is a description of my web page',
 }
 
 export default page;
