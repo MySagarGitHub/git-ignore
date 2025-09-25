d21b55c6425f7a5ef7ff18802003c820a8a6cfd6
MySagarGithub <Pandaysagar2004@gmail.com>
2025-09-25 22:06:26 +0545

    daily contribution

--- index.php
+++ index.php
@@ -3,4 +3,9 @@
 echo "Hello, World!";
 echo "This is a simple PHP application.";
 
+// Additional functionality can be added here
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    echo "Received data: " . print_r($data, true);
+}
 ?>
