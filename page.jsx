ebe212506eb47a7011c7f6e3973c442139db40b3
MySagarGithub <Pandaysagar2004@gmail.com>
2025-09-26 21:22:22 +0545

    daily contribution

--- page.jsx
+++ page.jsx
@@ -25,8 +25,12 @@ function MyForm() {
       </form>
       <div>
         <nav>
-          <a href="/">Home</a> | <a href="/about">About</a> | <a href="/contact">Contact</a>
-          <a href="/services">Services</a>  
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/services">Services</a></li>
+          </ul>
         </nav>
       </div>
     </div>
