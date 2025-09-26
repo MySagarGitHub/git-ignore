eb477627daafa5da68dc5e98e26088020124eac2
MySagarGithub <Pandaysagar2004@gmail.com>
2025-09-26 21:21:50 +0545

    daily contribution

--- TODO/app.js
+++ TODO/app.js
@@ -188,6 +188,7 @@
   window.todoApp = {
     add, remove, toggle, clearCompleted, getAll: () => todos
   };
+  
 })();
 
 
--- index.php
+++ index.php
@@ -7,5 +7,11 @@ echo "This is a simple PHP application.";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
     echo "Received data: " . print_r($data, true);
+    // Process the received data as needed
+    if (isset($data['name']) && isset($data['email'])) {
+        echo "Name: " . htmlspecialchars($data['name']) . ", Email: " . htmlspecialchars($data['email']);
+    } else {
+        echo "Invalid data received.";
+    }
 }
 ?>
